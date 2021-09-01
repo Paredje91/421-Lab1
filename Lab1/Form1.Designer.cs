2d5a9db5a8c2226b482df95a6dbf357778ca82bb
marias rax pc <marias rax pc@LAPTOP-PH0SCJP2>
2021-09-01 16:47:54 -0700

    Add project files.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_line = new System.Windows.Forms.Button();
+            this.btn_rec = new System.Windows.Forms.Button();
+            this.btn_ellipse = new System.Windows.Forms.Button();
+            this.redTrackBar = new System.Windows.Forms.TrackBar();
+            this.greenTrackBar = new System.Windows.Forms.TrackBar();
+            this.blueTrackBar = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_line
+            // 
+            this.btn_line.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_line.Location = new System.Drawing.Point(34, 12);
+            this.btn_line.Name = "btn_line";
+            this.btn_line.Size = new System.Drawing.Size(93, 33);
+            this.btn_line.TabIndex = 0;
+            this.btn_line.Text = "Line";
+            this.btn_line.UseVisualStyleBackColor = true;
+            this.btn_line.Click += new System.EventHandler(this.btn_line_Click);
+            // 
+            // btn_rec
+            // 
+            this.btn_rec.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_rec.Location = new System.Drawing.Point(133, 11);
+            this.btn_rec.Name = "btn_rec";
+            this.btn_rec.Size = new System.Drawing.Size(102, 34);
+            this.btn_rec.TabIndex = 1;
+            this.btn_rec.Text = "Rectangle";
+            this.btn_rec.UseVisualStyleBackColor = true;
+            this.btn_rec.Click += new System.EventHandler(this.btn_rec_Click);
+            // 
+            // btn_ellipse
+            // 
+            this.btn_ellipse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ellipse.Location = new System.Drawing.Point(241, 11);
+            this.btn_ellipse.Name = "btn_ellipse";
+            this.btn_ellipse.Size = new System.Drawing.Size(97, 34);
+            this.btn_ellipse.TabIndex = 2;
+            this.btn_ellipse.Text = "Ellipse";
+            this.btn_ellipse.UseVisualStyleBackColor = true;
+            this.btn_ellipse.Click += new System.EventHandler(this.btn_ellipse_Click);
+            // 
+            // redTrackBar
+            // 
+            this.redTrackBar.BackColor = System.Drawing.Color.Red;
+            this.redTrackBar.Location = new System.Drawing.Point(458, 11);
+            this.redTrackBar.Maximum = 255;
+            this.redTrackBar.Name = "redTrackBar";
+            this.redTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.redTrackBar.TabIndex = 3;
+            this.redTrackBar.Scroll += new System.EventHandler(this.redTrackBar_Scroll);
+            // 
+            // greenTrackBar
+            // 
+            this.greenTrackBar.BackColor = System.Drawing.Color.Lime;
+            this.greenTrackBar.Location = new System.Drawing.Point(593, 12);
+            this.greenTrackBar.Maximum = 255;
+            this.greenTrackBar.Name = "greenTrackBar";
+            this.greenTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.greenTrackBar.TabIndex = 4;
+            this.greenTrackBar.Scroll += new System.EventHandler(this.greenTrackBar_Scroll);
+            // 
+            // blueTrackBar
+            // 
+            this.blueTrackBar.BackColor = System.Drawing.Color.Blue;
+            this.blueTrackBar.Location = new System.Drawing.Point(721, 11);
+            this.blueTrackBar.Maximum = 255;
+            this.blueTrackBar.Name = "blueTrackBar";
+            this.blueTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.blueTrackBar.TabIndex = 5;
+            this.blueTrackBar.Scroll += new System.EventHandler(this.blueTrackBar_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(12, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1140, 400);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 485);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.blueTrackBar);
+            this.Controls.Add(this.greenTrackBar);
+            this.Controls.Add(this.redTrackBar);
+            this.Controls.Add(this.btn_ellipse);
+            this.Controls.Add(this.btn_rec);
+            this.Controls.Add(this.btn_line);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_line;
+        private System.Windows.Forms.Button btn_rec;
+        private System.Windows.Forms.Button btn_ellipse;
+        private System.Windows.Forms.TrackBar redTrackBar;
+        private System.Windows.Forms.TrackBar greenTrackBar;
+        private System.Windows.Forms.TrackBar blueTrackBar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        Bitmap bm;
+        Graphics g;
+        Color color = new Color();
+
+        int oldX, oldY, newX, newY, calcX, calcY, x, y;
+        bool paint = false;
+        Pen pen = new Pen(Color.Black);
+        int buttonNum = 1;
+        int blueVal, greenVal, redVal;
+
+        private void btn_rec_Click(object sender, EventArgs e)
+        {
+            buttonNum = 2;
+        }
+
+        private void btn_ellipse_Click(object sender, EventArgs e)
+        {
+            buttonNum = 3;
+        }
+
+        private void btn_line_Click(object sender, EventArgs e)
+        {
+            buttonNum = 1;
+        }
+
+        private void greenTrackBar_Scroll(object sender, EventArgs e)
+        {
+            greenVal = greenTrackBar.Value;
+        }
+
+        private void blueTrackBar_Scroll(object sender, EventArgs e)
+        {
+            blueVal = blueTrackBar.Value;
+        }
+
+        private void redTrackBar_Scroll(object sender, EventArgs e)
+        {
+            redVal = redTrackBar.Value;
+        }
+
+        
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            
+            paint = true;
+            oldX = e.X;
+            oldY = e.Y;
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        { 
+            
+            paint = false;
+
+
+            
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            newX = e.X;
+            newY = e.Y;
+
+            calcX = Math.Abs(newX - oldX);
+            calcY = Math.Abs(oldY - newY);
+
+            pen.Color = Color.FromArgb(redVal, greenVal, blueVal);
+
+            if (paint)
+            {
+                if (buttonNum == 1)
+                {
+                    g.DrawLine(pen, oldX, oldY, newX, newY);
+
+                }
+
+                if (buttonNum == 2)
+                {
+                    g.DrawRectangle(pen, oldX, oldY, calcX, calcY);
+                }
+
+                if (buttonNum == 3)
+                {
+                    g.DrawEllipse(pen, oldX, oldY, calcX, calcY);
+                }
+
+            }
+            
+           
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+
+
+        }
+
+
+    }
+}
